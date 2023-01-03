f7bb5b20b12e5daa3a19aa994a02a07f53257f69
Patrick Frueh <patrick.frueh@gmx.net>
2023-01-03 20:44:21 +0100

    Protoype test run for one match

--- JavaScript/rock-paper-scissors/rps.js
+++ JavaScript/rock-paper-scissors/rps.js
@@ -1,7 +1,4 @@
-/* Helper functions */
-
-
-/* Problem related function */
+/* Randomly generated choice */
 function getComputerChoice() {
     /* Randomly generate number: 0, 1, 2 
        Pick computer choice based on number */
@@ -23,3 +20,50 @@ function getComputerChoice() {
     return choice;
 }
 
+
+/* Play a round of RPS */
+function playMatch(computerChoice, playerChoice) {
+    switch (computerChoice) {
+        case 'Rock':
+            switch (playerChoice) {
+                case 'Rock':
+                    result = 'Even';
+                    break;
+                case 'Paper':
+                    result = 'Player wins!';
+                    break;
+                case 'Scissors':
+                    result = 'Computer wins!';
+                    break;
+            }
+        case 'Paper':
+            switch (playerChoice) {
+                case 'Rock':
+                    result = 'Computer wins!';
+                    break;
+                case 'Paper':
+                    result = 'Even';
+                    break;
+                case 'Scissors':
+                    result = 'Player wins!';
+                    break;
+            }
+        case 'Scissors':
+            switch (playerChoice) {
+                case 'Rock':
+                    result = 'Player wins!';
+                    break;
+                case 'Paper':
+                    result = 'Computer wins';
+                    break;
+                case 'Scissors':
+                    result = 'Even';
+                    break;
+            }
+    }
+    return result;
+}
+
+const playerSelection = "Scissors"
+const computerSelection = getComputerChoice()
+console.log(playMatch(computerSelection, playerSelection))
