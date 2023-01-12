3020c78c68535b101f3599f3ca304ff7aade5b0a
Patrick Frueh <patrick.frueh@gmx.net>
2023-01-12 19:27:55 +0100

    Pass tests for 02

--- JavaScript/javascript-exercises/02_repeatString/repeatString.js
+++ JavaScript/javascript-exercises/02_repeatString/repeatString.js
@@ -1,4 +1,10 @@
 const repeatString = function (string, num) {
+    // Check for a negative number
+    if (num < 0) {
+        return 'ERROR'
+    }
+
+    // Repeat the input string a *num* amount of times
     let repeatedString = ''
     for (i = 0; i < num; i++) {
         repeatedString += string
