cd288655f599d5b6ddb3bf3a293d44df1c3064ac
Patrick Frueh <patrick.frueh@gmx.net>
2023-01-12 19:24:02 +0100

    Pass the first test

--- JavaScript/javascript-exercises/02_repeatString/repeatString.js
+++ JavaScript/javascript-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    let repeatedString = ''
+    for (i = 0; i < num; i++) {
+        repeatedString += string
+    }
+    return repeatedString
 };
 
 // Do not edit below this line
