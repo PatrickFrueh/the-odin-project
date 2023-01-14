0984c04b4d53e6d284808ca7d3054f626f972d27
Patrick Frueh <patrick.frueh@gmx.net>
2023-01-14 21:11:30 +0100

    Pass tests 04

--- JavaScript/javascript-exercises/04_removeFromArray/removeFromArray.js
+++ JavaScript/javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...args) {
+    for (j = 0; j < args.length; j++) {
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i] === args[j]) {
+                arr.splice(i, 1);
+            }
+        }
+    }
+    return arr
 };
+
+removeFromArray([1, 2, 3, 4], 3, 2)
 
 // Do not edit below this line
 module.exports = removeFromArray;
