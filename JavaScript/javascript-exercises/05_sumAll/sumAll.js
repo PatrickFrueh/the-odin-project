c4b4a4f6b32fee503f352df839aaf9269d8774b3
Patrick Frueh <patrick.frueh@gmx.net>
2023-01-14 21:41:28 +0100

    Pass tests 05

--- JavaScript/javascript-exercises/05_sumAll/sumAll.js
+++ JavaScript/javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (firstNumber, secondNumber) {
+    // Check for negative, order, ...
+    if (((firstNumber || secondNumber) < 0) || !Number.isInteger(firstNumber) || !Number.isInteger(secondNumber)) {
+        return 'ERROR'
+    }
+    else if (firstNumber > secondNumber) {
+        bigNumber = firstNumber;
+        smallNumber = secondNumber;
+    }
+    else if (firstNumber < secondNumber) {
+        bigNumber = secondNumber;
+        smallNumber = firstNumber;
+    };
 
+    // Initialize variables
+    let iterations = bigNumber - smallNumber; // 4 - 1 = 3
+    let sumAll = 0;
+
+    for (i = smallNumber; i <= (iterations + 1); i++) {
+        sumAll += i;
+    }
+
+    return sumAll
 };
 
 // Do not edit below this line
