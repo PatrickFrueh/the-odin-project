2ce8f6ad6ea6862acf2b25fa890fbd3463a34320
Patrick Frueh <patrick.frueh@gmx.net>
2023-01-19 20:20:03 +0100

    Pass tests for 07

--- JavaScript/javascript-exercises/07_tempConversion/tempConversion.js
+++ JavaScript/javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  // Convert entered fahrenheit to celsius
+  let celsius = 0;
+  celsius = (fahrenheit - 32) * (5 / 9);
+  celsius = Math.round(celsius * 10) / 10;
+  return celsius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  // Convert entered celsius to fahrenheit
+  let fahrenheit = 0;
+  fahrenheit = (celsius * (9 / 5) + 32);
+  fahrenheit = Math.round(fahrenheit * 10) / 10;
+  return fahrenheit
 };
 
 // Do not edit below this line
