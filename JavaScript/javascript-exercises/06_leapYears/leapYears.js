1331f7c3bd7dccb45ccfecd269e7d56a153fe407
Patrick Frueh <patrick.frueh@gmx.net>
2023-01-19 20:10:08 +0100

    Pass tests for 06

--- JavaScript/javascript-exercises/06_leapYears/leapYears.js
+++ JavaScript/javascript-exercises/06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+// Check if entered year is a leap year
+const leapYears = function (year) {
+    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+        return true
+    }
+    else {
+        return false
+    }
 };
 
 // Do not edit below this line
